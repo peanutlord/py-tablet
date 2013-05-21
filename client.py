52ea45a423924e300c41f96072d754cda9f71fd3
Christopher Marchfelder <marchfelder@googlemail.com>
2013-05-21 09:49:01 +0200

    Replaced IP

--- client.py
+++ client.py
@@ -2,6 +2,8 @@ __author__ = 'chris'
 
 import xmlrpclib
 
-proxy = xmlrpclib.ServerProxy("http://192.168.178.33:9999")
+# Test File
+
+proxy = xmlrpclib.ServerProxy("http://localhost:9999")
 # proxy.openUri('spotify:track:3cQGb2POE359G9WH81bF60')
 proxy.getVolume()
